78063f194d883c5189cea46d0c91cfecb6bcd11a
Than Dao <daothan1211@gmail.com>
2024-02-21 15:57:14 +0700

    Update

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
 # Stop all servers and start the server as a daemon
-cd /app/current
-
 pm2 kill
 npm run pm2-start
--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -3,5 +3,4 @@
 npm install -g pm2
 npm install -g npx
 
-cd /app/current
 npm install
